9ac2afeb7868a9b67898a13a6b030d8473a2ce07
Jan Stampfli <jan.stampfli@hslu.ch>
2020-05-11 16:22:16 +0200

    Added outlet temperature calculation and test

--- heat_exchanger.py
+++ heat_exchanger.py
@@ -1,15 +1,34 @@
 
 class HeatExchanger:
-    """Class for heat exchanger calculation"""
+    """Class for heat exchanger calculation. First index of list is hot stream, and second index is cold stream. The following units are used:
+        [temperatures] = K
+        [film_heat_transfer_coefficients] = kW/(m2K)
+        [heat_capacity_flows = specific_heat_transfer_coefficient * mass_flow] = kW/s 
+        [heat_load] = kW"""
 
-    def __init__(self, film_heat_transfer_coefficient_hot_side, film_heat_transfer_coefficient_cold_side):
-
-        self.film_heat_treansfer_coefficient_hot_stream = film_heat_transfer_coefficient_hot_side
-        self.film_heat_treansfer_coefficient_cold_stream = film_heat_transfer_coefficient_cold_side
+    def __init__(self, inlet_temperatures, film_heat_transfer_coefficients, heat_capacity_flows, heat_load):
+        self.inlet_temperature_hot_stream = inlet_temperatures[0]
+        self.inlet_temperature_cold_stream = inlet_temperatures[1]
+        self.film_heat_treansfer_coefficient_hot_stream = film_heat_transfer_coefficients[0]
+        self.film_heat_treansfer_coefficient_cold_stream = film_heat_transfer_coefficients[1]
+        self.heat_capacity_flow_hot_stream = heat_capacity_flows[0]
+        self.heat_capacity_flow_cold_stream = heat_capacity_flows[1]
+        self.heat_load = heat_load
 
     @property
     def overall_heat_treansfer_coefficient(self):
         return 1 / (1 / self.film_heat_treansfer_coefficient_hot_stream + 1 / self.film_heat_treansfer_coefficient_cold_stream)
+
+    @property
+    def outlet_temperature_hot_stream(self):
+        return self.inlet_temperature_hot_stream - self.stream_temperature_difference(self.heat_capacity_flow_hot_stream)
+
+    @property
+    def outlet_temperature_cold_stream(self):
+        return self.inlet_temperature_cold_stream + self.stream_temperature_difference(self.heat_capacity_flow_cold_stream)
+
+    def stream_temperature_difference(self, heat_capacity_flow):
+        return self.heat_load / heat_capacity_flow
 
     def __repr__(self):
         pass
--- test_heat_exchanger.py
+++ test_heat_exchanger.py
@@ -4,14 +4,30 @@ from heat_exchanger import HeatExchanger
 class TestHeatExchanger:
     """Unit tests for heat exchanger calculation"""
 
+    def setup_model(self):
+        """Setup coefficients for testing the heat exchanger class"""
+        self.inlet_temperatures = [80, 20]
+        self.film_heat_transfer_coefficients = [1500, 3000]
+        self.heat_capacity_flows = [15, 20]
+        self.heat_load = 25
+        self.heat_exchanger = HeatExchanger(
+            self.inlet_temperatures, self.film_heat_transfer_coefficients, self.heat_capacity_flows, self.heat_load)
+
     def test_overall_heat_transfer_coefficient(self):
-        film_heat_transfer_coefficient_hot_stream, film_heat_transfer_coefficient_cold_stream = 1500, 3000
+        self.setup_model()
         overall_heat_transfer_coefficient = 1 / \
-            (1 / film_heat_transfer_coefficient_hot_stream +
-             1 / film_heat_transfer_coefficient_cold_stream)
-        heat_exchanger = HeatExchanger(
-            film_heat_transfer_coefficient_hot_stream, film_heat_transfer_coefficient_cold_stream)
-        assert overall_heat_transfer_coefficient == heat_exchanger.overall_heat_treansfer_coefficient
+            (1 / self.film_heat_transfer_coefficients[0] +
+             1 / self.film_heat_transfer_coefficients[1])
+        assert overall_heat_transfer_coefficient == self.heat_exchanger.overall_heat_treansfer_coefficient
+
+    def test_outlet_temperatures(self):
+        self.setup_model()
+        outlet_temperature_hot_stream = self.inlet_temperatures[0] - \
+            self.heat_load / self.heat_capacity_flows[0]
+        outlet_temperature_cold_stream = self.inlet_temperatures[1] + \
+            self.heat_load / self.heat_capacity_flows[1]
+        assert outlet_temperature_hot_stream == self.heat_exchanger.outlet_temperature_hot_stream
+        assert outlet_temperature_cold_stream == self.heat_exchanger.outlet_temperature_cold_stream
 
 
 if __name__ == "__main__":
