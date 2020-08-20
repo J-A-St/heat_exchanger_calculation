bd4ba98cf7880e871d8afec467aa6b5c8ce0b287
Jan Stampfli <jan.stampfli@hslu.ch>
2020-08-20 15:37:43 +0200

    Added bypassing and admixing feature for HEX calculation

--- heat_exchanger.py
+++ heat_exchanger.py
@@ -1,17 +1,23 @@
-import math
+import numpy as np
 
 
 class HeatExchanger:
     """Class for heat exchanger calculation
 
         Arguments:
-            inlet_temperatures {float} -- List of inlet temperatures (°C) with hot stream [0] and cold stream [1] 
+            inlet_temperatures {float} -- List of inlet temperatures (°C) with hot stream [0] and cold stream [1]
             film_heat_transfer_coefficients {float} -- List of film heat transfer coefficients(kW/(m2K) with hot stream [0] and cold stream [1]
             heat_capacity_flows {float} -- List of heat capacity flows (kW/K) with hot stream [0] and cold stream [1]
             heat_load {float} -- Heat load (kW)
+            mixer_type_hot {string} -- none, bypass, or admixer
+            mixer_type_cold {string} -- none, bypass, or admixer
+            mixer_fraction_hot {float} -- 0...1 ((kg/s)/(kg/s))
+            mixer_fraction_cold {float} -- 0...1 ((kg/s)/(kg/s))
         Properties:
             inlet_temperature_hot_stream {float} -- inlet temperature hot stream (°C)
             inlet_temperature_cold_stream {float} -- inlet temperature cold stream (°C)
+            heat_exchanger_inlet_temperature_hot_stream {float} -- inlet temperature hot stream in mixer (°C)
+            heat_exchanger_inlet_temperature_cold_stream {float} -- inlet temperature cold stream in mixer (°C)
             film_heat_transfer_coefficient_hot_stream {float} -- Film heat transfer coefficient hot stream (kW/(m2K))
             film_heat_transfer_coefficient_cold_stream {float} -- Film heat transfer coefficient cold stream (kW/(m2K))
             heat_capacity_flow_hot_stream {float} -- Heat capacity flow hot stream (kW/K)
@@ -24,7 +30,11 @@ class HeatExchanger:
             area {float} --Resulting area (m2)
         """
 
-    def __init__(self, inlet_temperatures, film_heat_transfer_coefficients, heat_capacity_flows, heat_load):
+    def __init__(self, inlet_temperatures, film_heat_transfer_coefficients, heat_capacity_flows, heat_load, mixer_type_hot='none', mixer_type_cold='none', mixer_fraction_hot=0, mixer_fraction_cold=0):
+        self.mixer_type_hot = mixer_type_hot
+        self.mixer_type_cold = mixer_type_cold
+        self.mixer_fraction_hot = mixer_fraction_hot
+        self.mixer_fraction_cold = mixer_fraction_cold
         self.inlet_temperature_hot_stream = inlet_temperatures[0]
         self.inlet_temperature_cold_stream = inlet_temperatures[1]
         self.film_heat_transfer_coefficient_hot_stream = film_heat_transfer_coefficients[0]
@@ -38,28 +48,71 @@ class HeatExchanger:
         return 1 / (1 / self.film_heat_transfer_coefficient_hot_stream + 1 / self.film_heat_transfer_coefficient_cold_stream)
 
     @property
+    def heat_exchanger_inlet_temperature_hot_stream(self):
+        return self.heat_exchanger_inlet_temperature_calculation(self.inlet_temperature_hot_stream, self.heat_capacity_flow_hot_stream, self.mixer_type_hot, self.mixer_fraction_hot, 'hot')
+
+    @property
+    def heat_exchanger_inlet_temperature_cold_stream(self):
+        return self.heat_exchanger_inlet_temperature_calculation(self.inlet_temperature_cold_stream, self.heat_capacity_flow_cold_stream, self.mixer_type_cold, self.mixer_fraction_cold, 'cold')
+
+    @property
+    def heat_exchanger_outlet_temperature_hot_stream(self):
+        return self.heat_exchanger_outlet_temperature_calculation(self.heat_exchanger_inlet_temperature_hot_stream, self.heat_capacity_flow_hot_stream, self.mixer_type_hot, self.mixer_fraction_hot, 'hot')
+
+    @property
+    def heat_exchanger_outlet_temperature_cold_stream(self):
+        return self.heat_exchanger_outlet_temperature_calculation(self.heat_exchanger_inlet_temperature_cold_stream, self.heat_capacity_flow_cold_stream, self.mixer_type_cold, self.mixer_fraction_cold, 'cold')
+
+    @property
     def outlet_temperature_hot_stream(self):
-        return self.inlet_temperature_hot_stream - self.stream_temperature_difference(self.heat_capacity_flow_hot_stream)
+        return self.inlet_temperature_hot_stream + self.stream_temperature_difference(self.heat_capacity_flow_hot_stream, 'none', 0, 'hot')
 
     @property
     def outlet_temperature_cold_stream(self):
-        return self.inlet_temperature_cold_stream + self.stream_temperature_difference(self.heat_capacity_flow_cold_stream)
+        return self.inlet_temperature_cold_stream + self.stream_temperature_difference(self.heat_capacity_flow_cold_stream, 'none', 0, 'cold')
 
     @property
     def logarithmic_temperature_difference(self):
-        temperature_difference_a = self.outlet_temperature_hot_stream - self.inlet_temperature_cold_stream
-        temperature_difference_b = self.inlet_temperature_hot_stream - self.outlet_temperature_cold_stream
+        temperature_difference_a = self.heat_exchanger_outlet_temperature_hot_stream - self.heat_exchanger_inlet_temperature_cold_stream
+        temperature_difference_b = self.heat_exchanger_inlet_temperature_hot_stream - self.heat_exchanger_outlet_temperature_cold_stream
         if temperature_difference_a == temperature_difference_b:
             return temperature_difference_a
         else:
-            return (temperature_difference_a - temperature_difference_b) / math.log(temperature_difference_a / temperature_difference_b)
+            return (temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
 
     @property
     def area(self):
         return self.heat_load / (self.overall_heat_transfer_coefficient * self.logarithmic_temperature_difference)
 
-    def stream_temperature_difference(self, heat_capacity_flow):
-        return self.heat_load / heat_capacity_flow
+    def heat_exchanger_inlet_temperature_calculation(self, inlet_temperature, heat_capacity_flow, mixer_type, mixer_fraction, stream_type):
+        if mixer_type == 'bypass' or mixer_type == 'none':
+            return inlet_temperature
+
+        elif mixer_type == 'admixer':
+            return (inlet_temperature + (inlet_temperature + self.stream_temperature_difference(heat_capacity_flow, 'none', 0, stream_type)) * mixer_fraction) / (1 + mixer_fraction)
+
+        else:
+            raise Exception("Sorry, you've misspelled the mixer type")
+
+    def heat_exchanger_outlet_temperature_calculation(self, heat_exchanger_inlet_temperature, heat_capacity_flow, mixer_type, mixer_fraction, stream_type):
+        return heat_exchanger_inlet_temperature + self.stream_temperature_difference(heat_capacity_flow, mixer_type, mixer_fraction, stream_type)
+
+    def stream_temperature_difference(self, heat_capacity_flow, mixer_type, mixer_fraction, stream_type):
+        if stream_type == 'hot':
+            stream_type_sign = -1
+        elif stream_type == 'cold':
+            stream_type_sign = 1
+        else:
+            raise Exception("Sorry,you've misspelled the stream type")
+
+        if mixer_type == 'bypass':
+            return stream_type_sign * self.heat_load / (heat_capacity_flow * (1 - mixer_fraction))
+        elif mixer_type == 'admixer':
+            return stream_type_sign * self.heat_load / (heat_capacity_flow * (1 + mixer_fraction))
+        elif mixer_type == 'none':
+            return stream_type_sign * self.heat_load / heat_capacity_flow
+        else:
+            raise Exception("Sorry, you've misspelled the mixer type")
 
     def __repr__(self):
         pass
--- test_heat_exchanger.py
+++ test_heat_exchanger.py
@@ -1,10 +1,15 @@
-import math
+import numpy as np
 
 from heat_exchanger import HeatExchanger
 
-inlet_temperatures = [80, 20]
-film_heat_transfer_coefficients = [1.5, 3]
-heat_capacity_flows = [15, 20]
+inlet_temperatures = [80.0, 20.0]
+film_heat_transfer_coefficients = [1, 1]
+heat_capacity_flows = [5, 2.5]
+heat_load = 100
+mixer_type_hot = 'none'
+mixer_type_cold = 'none'
+mixer_fraction_hot = 0
+mixer_fraction_cold = 0
 
 
 def setup_model():
@@ -13,7 +18,11 @@ def setup_model():
         inlet_temperatures=inlet_temperatures,
         film_heat_transfer_coefficients=film_heat_transfer_coefficients,
         heat_capacity_flows=heat_capacity_flows,
-        heat_load=25)
+        heat_load=heat_load,
+        mixer_type_hot=mixer_type_hot,
+        mixer_type_cold=mixer_type_cold,
+        mixer_fraction_hot=mixer_fraction_hot,
+        mixer_fraction_cold=mixer_fraction_cold)
 
 
 def test_overall_heat_transfer_coefficient():
@@ -34,7 +43,7 @@ def test_outlet_temperatures():
     assert outlet_temperature_cold_stream == m.outlet_temperature_cold_stream
 
 
-def test_logarithmic_temperature_difference():
+def test_logarithmic_temperature_difference_no_mixer():
     m = setup_model()
     temperature_difference_a = inlet_temperatures[0] - \
         m.outlet_temperature_cold_stream
@@ -44,7 +53,7 @@ def test_logarithmic_temperature_difference():
         logarithmic_temperature_difference = temperature_difference_a
     else:
         logarithmic_temperature_difference = (
-            temperature_difference_a - temperature_difference_b) / math.log(temperature_difference_a / temperature_difference_b)
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
     assert abs(logarithmic_temperature_difference -
                m.logarithmic_temperature_difference) <= 10e-10
 
@@ -55,8 +64,145 @@ def test_logarithmic_temperature_difference():
         m.outlet_temperature_cold_stream
 
 
-def test_area():
+def test_area_no_mixer():
     m = setup_model()
     area = m.heat_load / (m.overall_heat_transfer_coefficient *
                           m.logarithmic_temperature_difference)
     assert area == m.area
+
+
+def test_heat_exchanger_temperatures_bypass_hot_side():
+    m = setup_model()
+    m.mixer_type_hot = 'bypass'
+    m.mixer_fraction_hot = 0.2
+    assert m.heat_exchanger_inlet_temperature_hot_stream == inlet_temperatures[0]
+    assert m.heat_exchanger_outlet_temperature_hot_stream == inlet_temperatures[0] - heat_load / (heat_capacity_flows[0] * (1 - m.mixer_fraction_hot))
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / (heat_capacity_flows[0] * (1 - m.mixer_fraction_hot))) - inlet_temperatures[1]
+    temperature_difference_b = inlet_temperatures[0] - (inlet_temperatures[1] + heat_load / heat_capacity_flows[1])
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_bypass_cold_side():
+    m = setup_model()
+    m.mixer_type_cold = 'bypass'
+    m.mixer_fraction_cold = 0.2
+    assert m.heat_exchanger_inlet_temperature_cold_stream == inlet_temperatures[1]
+    assert m.heat_exchanger_outlet_temperature_cold_stream == inlet_temperatures[1] + heat_load / (heat_capacity_flows[1] * (1 - m.mixer_fraction_cold))
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / heat_capacity_flows[0]) - inlet_temperatures[1]
+    temperature_difference_b = inlet_temperatures[0] - (inlet_temperatures[1] + heat_load / (heat_capacity_flows[1] * (1 - m.mixer_fraction_cold)))
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_bypass_both_sides():
+    m = setup_model()
+    m.mixer_type_hot = 'bypass'
+    m.mixer_fraction_hot = 0.2
+    m.mixer_type_cold = 'bypass'
+    m.mixer_fraction_cold = 0.2
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / (heat_capacity_flows[0] * (1 - m.mixer_fraction_hot))) - inlet_temperatures[1]
+    temperature_difference_b = inlet_temperatures[0] - (inlet_temperatures[1] + heat_load / (heat_capacity_flows[1] * (1 - m.mixer_fraction_cold)))
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_admixer_hot_side():
+    m = setup_model()
+    m.mixer_type_hot = 'admixer'
+    m.mixer_fraction_hot = 0.2
+    assert m.heat_exchanger_inlet_temperature_hot_stream == (inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot)
+    assert m.heat_exchanger_outlet_temperature_hot_stream == (inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot) - heat_load / (heat_capacity_flows[0] * (1 + m.mixer_fraction_hot))
+    assert m.heat_exchanger_outlet_temperature_hot_stream == inlet_temperatures[0] - heat_load / heat_capacity_flows[0]
+    temperature_difference_a = (inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot) - heat_load / (heat_capacity_flows[0] * (1 + m.mixer_fraction_hot)) - inlet_temperatures[1]
+    temperature_difference_b = (inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot) - (inlet_temperatures[1] + heat_load / heat_capacity_flows[1])
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_admixer_cold_side():
+    m = setup_model()
+    m.mixer_type_cold = 'admixer'
+    m.mixer_fraction_cold = 0.2
+    assert m.heat_exchanger_inlet_temperature_cold_stream == (inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold)
+    assert m.heat_exchanger_outlet_temperature_cold_stream == (inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold) + heat_load / (heat_capacity_flows[1] * (1 + m.mixer_fraction_cold))
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / heat_capacity_flows[0]) - (inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold)
+    temperature_difference_b = inlet_temperatures[0] - ((inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold) + heat_load / (heat_capacity_flows[1] * (1 + m.mixer_fraction_cold)))
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_admixer_both_sides():
+    m = setup_model()
+    m.mixer_type_hot = 'admixer'
+    m.mixer_fraction_hot = 0.2
+    m.mixer_type_cold = 'admixer'
+    m.mixer_fraction_cold = 0.2
+    temperature_difference_a = ((inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot) - heat_load / (heat_capacity_flows[0] * (1 + m.mixer_fraction_hot))) - ((inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold))
+    temperature_difference_b = ((inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot)) - ((inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold) + heat_load / (heat_capacity_flows[1] * (1 + m.mixer_fraction_cold)))
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_bypass_hot_side_admixer_cold_side():
+    m = setup_model()
+    m.mixer_type_hot = 'bypass'
+    m.mixer_fraction_hot = 0.2
+    m.mixer_type_cold = 'admixer'
+    m.mixer_fraction_cold = 0.2
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / (heat_capacity_flows[0] * (1 - m.mixer_fraction_hot))) - ((inlet_temperatures[1] + m.outlet_temperature_cold_stream * m.mixer_fraction_cold) / (1 + m.mixer_fraction_cold))
+    temperature_difference_b = inlet_temperatures[0] - (inlet_temperatures[1] + heat_load / heat_capacity_flows[1])
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
+
+
+def test_heat_exchanger_temperatures_admixer_hot_side_bypass_cold_side():
+    m = setup_model()
+    m.mixer_type_hot = 'admixer'
+    m.mixer_fraction_hot = 0.2
+    m.mixer_type_cold = 'bypass'
+    m.mixer_fraction_cold = 0.2
+    temperature_difference_a = (inlet_temperatures[0] - heat_load / heat_capacity_flows[0]) - inlet_temperatures[1]
+    temperature_difference_b = ((inlet_temperatures[0] + m.outlet_temperature_hot_stream * m.mixer_fraction_hot) / (1 + m.mixer_fraction_hot)) - (inlet_temperatures[1] + heat_load / (heat_capacity_flows[1] * (1 - m.mixer_fraction_cold)))
+    if temperature_difference_a == temperature_difference_b:
+        logarithmic_temperature_difference = temperature_difference_a
+    else:
+        logarithmic_temperature_difference = (
+            temperature_difference_a - temperature_difference_b) / np.log(temperature_difference_a / temperature_difference_b)
+    assert abs(logarithmic_temperature_difference -
+               m.logarithmic_temperature_difference) <= 10e-10
